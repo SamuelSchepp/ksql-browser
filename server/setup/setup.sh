f8317099f2218bc7c7d1802b77a038ea1a366e3e
Samuel Schepp <samuel.schepp@mni.thm.de>
2018-05-21 12:42:07 +0200

    updated setup

--- server/setup/setup.sh
+++ server/setup/setup.sh
@@ -2,7 +2,7 @@
 CDIR=$(dirname "$0")
 
 kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic users
-
 kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic pageviews
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic twitter_cloud
 
 cat "$CDIR/setup.ksql" | ksql
